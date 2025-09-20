a7336bb45ee10c15eb2e94c51f1a19ee3544556d
CoruptedUsername <52797912+CoruptedUsername@users.noreply.github.com>
2025-09-20 17:23:29 -0500

    Cottonee Cove Replays v0.0.7d - Initial Upload

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
     outDir: "../pokemon-showdown-client/play.pokemonshowdown.com/replays",
     rollupOptions: {
       input: {
-        client: 'testclient-beta.html',
+        client: '../pokemon-showdown-client/play.pokemonshowdown.com/testclient-beta.html',
       },
       output: {
         manualChunks: {
